bdfb8699411cc1ca22f51dd7cb1092347755d99c
curedas-c <codjocuredas@gmail.com>
2022-04-06 11:45:55 +0000

    feat: adds ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,21 @@ import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
 
-const MODULES = [LoadingBarModule, LoadingBarHttpClientModule];
+const MODULES = [
+  LoadingBarModule,
+  LoadingBarHttpClientModule,
+  NgxsModule.forRoot([]),
+  NgxsLoggerPluginModule.forRoot(),
+  NgxsReduxDevtoolsPluginModule.forRoot(),
+  NgxsStoragePluginModule.forRoot(),
+  NgxsFormPluginModule.forRoot(),
+];
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ...MODULES],
